38fdb5f4435a2ecd1e25a66f2b8e5206136ddff3
wlsf82 <wlsf82@gmail.com>
2016-02-29 15:34:39 -0300

    First project commit.

---
+++ protractor.conf.js
@@ -0,0 +1,26 @@
+exports.config = {
+  // The address of a running selenium server.
+  seleniumAddress: 'http://ondemand.saucelabs.com:80/wd/hub',
+
+  // Spec patterns are relative to the location of this config.
+  specs: [
+    'spec/*_spec.js'
+  ],
+
+  capabilities: {
+    'browserName': 'chrome',
+    'chromeOptions': {'args': ['--disable-extensions']}
+
+    // Saucelabs credentials
+    username: 'talkabouttest',
+    accessKey: '397a4689-88ed-43c7-b57d-ce1332ca92a1'
+  },
+
+  jasmineNodeOpts: {
+    onComplete: null,
+    isVerbose: false,
+    showColors: true,
+    includeStackTrace: true,
+    defaultTimeoutInterval: 10000
+  }
+};
